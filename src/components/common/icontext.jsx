25a07ff4b860308b37683da7733b1793b40827a5
23-101139-alt <23-101139@students.eui.edu.eg>
2026-02-20 22:37:40 +0200

    sec5-responsive

--- src/components/common/icontext.jsx
+++ src/components/common/icontext.jsx
@@ -5,9 +5,11 @@ import '../common/icontext.css'
 const Icontext = (props) => {
     return ( 
         <article className='iconcontainer-sec2'>
+            <div className='icon-container2'>
             <figure className='icon-container'>
                 <img src={props.icon1} alt='icon'></img>
             </figure>
+            </div>
             <article className='text-part-column'>
                 <h2 className='title-icon'>{props.title}</h2>
                  <p className='para-icon'>{props.parag}</p>
